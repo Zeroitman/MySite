cf11e7a2232f22cefb68207a7b156d645f9832e7
armani <razzarioa@gmail.com>
2019-04-12 14:29:54 +0600

    Добавил поля даты: создано, отредоктировано, удалено, функцию строкового представления модели. Выполнено к тикету #4

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class Program:
     name = models.CharField(max_length=255)
     description = models.TextField(max_length=2000, blank=True, null=True)
-    # child_id related to Child model, delete Cascade
-    # author_therapist_id related to User model delete Cascade
-    # attending_therapist_id related to User model delete Cascade
+    # child_id related to Child model, delete Protect
+    # author_therapist_id related to User model delete Protect
+    # attending_therapist_id related to User model delete Protect
+    created_date = models.DateTimeField(auto_now_add=True)
+    edited_date = models.DateTimeField(auto_now=True)
+    deleted_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
