50d54b92c1e66ffe875272bdc63e7a150272ad58
armani <razzarioa@gmail.com>
2019-04-12 13:57:37 +0600

    Создал модель программы, с соответствующими полями. Выполнено к тикету #4

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Program:
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, blank=True, null=True)
+    # child_id related to Child model, delete Cascade
+    # author_therapist_id related to User model delete Cascade
+    # attending_therapist_id related to User model delete Cascade
