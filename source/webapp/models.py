e5da7729a2c2c883c414d818cbe8acb55258c213
Maks <Rrnn941@gmail.com>
2019-04-12 14:19:52 +0600

    Даты переведены с DateField поля в DateTimeField, для отслеживания времени редактирования либо добавления ticket #3

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -12,9 +12,9 @@ class Children(models.Model):
     preferences = models.CharField(max_length=1000, blank=True, null=True)
     # first_parent = models.ForeignKey() TODO
     # second_parent = models.ForeignKey() TODO
-    add_date = models.DateField(auto_now_add=True)
-    edit_date = models.DateField(auto_now=True, null=True, blank=True)
-    delete_date = models.DateField(null=True, blank=True)
+    add_date = models.DateTimeField(auto_now_add=True)
+    edit_date = models.DateTimeField(auto_now=True, null=True, blank=True)
+    delete_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return "%s %s, %s" % (self.first_name, self.last_name, self.age)
