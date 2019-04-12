4181714afd3dabe956cfe1e1029da5c5f87b1d5f
Maks <Rrnn941@gmail.com>
2019-04-12 14:02:51 +0600

    Добавлена модель Children, с соответствием с тикетом #3

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Children(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    third_name = models.CharField(max_length=100, blank=True, null=True)
+    birthday = models.DateField(auto_now_add=False, null=True, blank=True)
+    age = models.IntegerField()
+    address = models.CharField(max_length=200, blank=True, null=True)
+    characteristic = models.CharField(max_length=1000, blank=True, null=True)
+    preferences = models.CharField(max_length=1000, blank=True, null=True)
+    # first_parent = models.ForeignKey() TODO
+    # second_parent = models.ForeignKey() TODO
+    add_date = models.DateField(auto_now=True)
+    edit_date = models.DateField(null=True, blank=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s %s, %s" % (self.first_name, self.last_name, self.age)
