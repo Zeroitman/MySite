cc5bab89056fed26ebee2afac7f680ae8ae7607f
Maks <Rrnn941@gmail.com>
2019-04-12 14:15:06 +0600

    Фикс полей дат в модели, автоматические проставление даты при добавлении и отслеживание редактирований, тикет #3

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -5,16 +5,16 @@ class Children(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     third_name = models.CharField(max_length=100, blank=True, null=True)
-    birthday = models.DateField(auto_now_add=False, null=True, blank=True)
-    age = models.IntegerField()
+    birthday = models.DateField(null=True, blank=True)
+    age = models.CharField(max_length=100)
     address = models.CharField(max_length=200, blank=True, null=True)
     characteristic = models.CharField(max_length=1000, blank=True, null=True)
     preferences = models.CharField(max_length=1000, blank=True, null=True)
     # first_parent = models.ForeignKey() TODO
     # second_parent = models.ForeignKey() TODO
-    add_date = models.DateField(auto_now=True)
-    edit_date = models.DateField(null=True, blank=True)
-    is_deleted = models.BooleanField(default=False)
+    add_date = models.DateField(auto_now_add=True)
+    edit_date = models.DateField(auto_now=True, null=True, blank=True)
+    delete_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return "%s %s, %s" % (self.first_name, self.last_name, self.age)
