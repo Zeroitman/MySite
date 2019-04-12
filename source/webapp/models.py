39395a123fc8b64f1702066a8f772c17e0d64bde
bak0903 <bak0903@mail.ru>
2019-04-12 14:29:20 +0600

    Создал модель пользователь, с соответствующими полями. Выполнено к тикету #1

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='client', verbose_name='Пользователь')
+    phone = models.CharField(max_length=50, verbose_name='Телефон')
+    delete_date = models.DateTimeField()
+    edit_date = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    def __str__(self):
+        return "%s %s" % (self.user.first_name, self.user.last_name)
