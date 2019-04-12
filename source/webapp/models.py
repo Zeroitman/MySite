d2b1560244c332d2c743dc277dffa8e43b72748e
Zeroitman <borsh130894@gmail.com>
2019-04-12 14:32:28 +0600

    Создал модель навыков, с соответствующими полями. Выполнено к тикету #5

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Skill(models.Model):
+    code_skill = models.CharField(max_length=5, verbose_name='Код навыка', default="Введите код навыка!")
+    name = models.CharField(max_length=255, verbose_name='Название навыка')
+    # code_category = models.ForeignKey// привязать к модели Категории
+    description = models.TextField(max_length=1000, null=True, blank=True, verbose_name='Описание навыка')
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания навыка')
+    date_update = models.DateTimeField(null=True, blank=True, verbose_name='Время редактирование навыка')
+    date_delete = models.DateTimeField(null=True, blank=True, verbose_name='Время удаления навыка')
+
+    def __str__(self):
+        return self.name
