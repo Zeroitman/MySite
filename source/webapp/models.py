21bf073b90ee15cfa88377bcfc55d3970e4abc59
Ermek K <kadyrkulove@gmail.com>
2019-04-12 14:21:03 +0600

    Создал модель "категории", Issue #2

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    code_category.Charfield(max_length=50, null=True, blank=True)
+    add_date = models.DateTimeField(auto_now_add=True)
+    edit_date = models.DateTimeField(auto_now=True, null=True, blank=True)
+    deleted_date = models.DateTimeField(null=True, blank=True)
+
+    def _str_(self):
+        return self.name
