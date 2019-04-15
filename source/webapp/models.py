94bee18b7b905fe5a8f61a3fef1e443edc7daccb
Maks <Rrnn941@gmail.com>
2019-04-15 12:34:22 +0600

    Готовая модель Ребенка, коммит для мерджа в мастера

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -7,14 +7,15 @@ class Children(models.Model):
     third_name = models.CharField(max_length=100, blank=True, null=True)
     birthday = models.DateField(null=True, blank=True)
     age = models.CharField(max_length=100)
-    address = models.CharField(max_length=200, blank=True, null=True)
+    address = models.TextField(max_length=1000, blank=True, null=True)
     characteristic = models.CharField(max_length=1000, blank=True, null=True)
     preferences = models.CharField(max_length=1000, blank=True, null=True)
-    # first_parent = models.ForeignKey() TODO
+    contacts = models.CharField(max_length=200, blank=True, null=True)
+    # first_parent = models.ForeignKey() TODO required#id
     # second_parent = models.ForeignKey() TODO
     add_date = models.DateTimeField(auto_now_add=True)
     edit_date = models.DateTimeField(auto_now=True, null=True, blank=True)
-    delete_date = models.DateTimeField(null=True, blank=True)
+    delete_date = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     def __str__(self):
         return "%s %s, %s" % (self.first_name, self.last_name, self.age)
