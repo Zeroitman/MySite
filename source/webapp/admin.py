450069cd4849c295c49204645b22f95099feb310
bak0903 <bak0903@mail.ru>
2019-04-16 23:56:43 +0600

    Провел миграацию и подключенил модели к админке. Выполнено к тикету #8

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from webapp.models import UserInfo, Skill, Program
+from webapp.models import UserInfo, Children, Category, Skill, Program, Session, Results
 
 
 admin.site.register(UserInfo)
+admin.site.register(Children)
+admin.site.register(Category)
 admin.site.register(Skill)
 admin.site.register(Program)
+admin.site.register(Session)
+admin.site.register(Results)
