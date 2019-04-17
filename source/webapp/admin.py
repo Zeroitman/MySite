34f325bdaf9dac307910a506df7f8568503a0f6e
armani <razzarioa@gmail.com>
2019-04-17 17:16:42 +0600

    Разбил urls.py на общий и urls.py приложения, создал view для вывода программ, подключил view в urls.py приложения, создал шаблон для вывода программ, выполнил к тикету #23

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from webapp.models import UserInfo, Skill, Program
+from webapp.models import UserInfo, Skill, Program, Session, Results, Children, Category
 
 
 admin.site.register(UserInfo)
 admin.site.register(Skill)
 admin.site.register(Program)
+admin.site.register(Session)
+admin.site.register(Results)
+admin.site.register(Children)
+admin.site.register(Category)
