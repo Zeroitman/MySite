93a21a6f1e76f60b79211aa20954e97649f7d4ff
Maks <Rrnn941@gmail.com>
2019-04-18 13:35:51 +0600

    Добавлена ссылка с ProgramList на ProgramDetail, вывод информации на странице просмотра делатей, сделано к тикету #26

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from webapp.views import ProgramList
+from webapp.views import ProgramList, ProgramDetailView
 
 
 app_name = 'webapp'
 urlpatterns = [
-    path('', ProgramList.as_view(), name='program_list')
+    path('', ProgramList.as_view(), name='program_list'),
+    path('program/<int:pk>', ProgramDetailView.as_view(), name='program_detail')
 ]
 
 
