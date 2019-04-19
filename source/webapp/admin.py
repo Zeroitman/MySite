e2e62953411e177c7f9106c702b35c6c12f85506
Maks <Rrnn941@gmail.com>
2019-04-19 13:06:07 +0600

    Переделан django admin, улучшен функционал. Добавлен функционал фильтрации данных по датам, поиска данных по ключевым словам, более понятный вывод данных, подправлены verbos_names и related names, удалены __str__ из моделей и добавлены class Metas.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,12 +1,78 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from webapp.models import UserInfo, Skill, Program, Session, Results, Children, Category
+from django.contrib.auth.admin import UserAdmin
 
 
-admin.site.register(UserInfo)
-admin.site.register(Skill)
-admin.site.register(Program)
-admin.site.register(Session)
-admin.site.register(Results)
-admin.site.register(Children)
-admin.site.register(Category)
+class ResultModelAdmin(admin.ModelAdmin):
+    list_display = ['session_id', 'code_skill']
+    search_fields = ['session_id', 'code_skill']
+    list_filter = ['created_date', 'edited_date']
 
+    def session_id(self, obj):
+        return obj.session.id
+    session_id.empty_value_display = 'Не известно'
+
+    def code_skill(self, obj):
+        return obj.skill.code_skill
+    code_skill.empty_value_display = 'Не известно'
+
+
+class SessionModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'program_name', 'created_date']
+    search_fields = ['program_name']
+    list_filter = ['created_date', 'edited_date']
+
+    def program_name(self, obj):
+        return obj.program.name
+
+    program_name.empty_value_display = 'Не известно'
+
+
+class ProgramModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'child_name']
+    search_fields = ['name', 'child_name']
+    list_filter = ['created_date', 'edited_date']
+    filter_horizontal = ('skill',)
+
+    def child_name(self, obj):
+        return obj.child.first_name
+    child_name.empty_value_display = 'Не известно'
+
+
+class SkillModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code_skill']
+    search_fields = ['name', 'code_skill']
+    list_filter = ['created_date', 'updated_date']
+
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code_category']
+    search_fields = ['name']
+    list_filter = ['created_date', 'edited_date', 'code_category']
+
+
+class ChildrenModelAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "age", "first_parent"]
+    search_fields = ["first_name"]
+    list_filter = ['created_date', 'edited_date']
+
+
+class InlineUser(admin.StackedInline):
+    model = UserInfo
+
+
+class UserInfoAdmin(UserAdmin):
+    inlines = [InlineUser, ]
+    search_fields = ['username']
+
+
+admin.site.site_header = 'Administrate your website'
+admin.site.unregister(User)
+admin.site.register(User, UserInfoAdmin)
+admin.site.register(Skill, SkillModelAdmin)
+admin.site.register(Program, ProgramModelAdmin)
+admin.site.register(Session, SessionModelAdmin)
+admin.site.register(Results, ResultModelAdmin)
+admin.site.register(Children, ChildrenModelAdmin)
+admin.site.register(Category, CategoryModelAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -9,8 +9,9 @@ class Category(models.Model):
     edited_date = models.DateTimeField(auto_now=True, null=True, blank=True, verbose_name='Время редактирования')
     deleted_date = models.DateTimeField(null=True, blank=True, verbose_name='Время удаления')
 
-    def _str_(self):
-        return "%s, Код категории: %s" % (self.name, self.code_category)
+    class Meta:
+        verbose_name = 'Категории'
+        verbose_name_plural = 'Категории'
 
 
 class Skill(models.Model):
@@ -19,11 +20,13 @@ class Skill(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='skill')
     description = models.TextField(max_length=1000, null=True, blank=True, verbose_name='Описание навыка')
     created_date = models.DateTimeField(auto_now_add=True, verbose_name='Время создания навыка')
-    updated_date = models.DateTimeField(auto_now=True, null=True, blank=True, verbose_name='Время редактирование навыка')
+    updated_date = models.DateTimeField(auto_now=True, null=True, blank=True,
+                                        verbose_name='Время редактирование навыка')
     deleted_date = models.DateTimeField(null=True, blank=True, verbose_name='Время удаления навыка')
 
-    def __str__(self):
-        return '%s Код навыка: %s' % (self.name, self.code_skill)
+    class Meta:
+        verbose_name = 'Навык'
+        verbose_name_plural = 'Навыки'
 
 
 class UserInfo(models.Model):
@@ -46,20 +49,23 @@ class Children(models.Model):
     characteristic = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Характеристика на ребенка')
     preferences = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Предпочтения ребенка')
     contacts = models.CharField(max_length=200, blank=True, null=True, verbose_name='Контакты ребенка')
-    first_parent = models.ForeignKey(UserInfo, on_delete=models.PROTECT, related_name='fp_child')
+    first_parent = models.ForeignKey(UserInfo, on_delete=models.PROTECT, verbose_name='Родитель',
+                                     related_name='fp_child')
     second_parent = models.ForeignKey(UserInfo, on_delete=models.PROTECT, related_name='sp_child')
     created_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления ребенка')
     edited_date = models.DateTimeField(auto_now=True, null=True, blank=True, verbose_name='Дата редактирования')
     deleted_date = models.DateTimeField(null=True, blank=True, verbose_name='Дата удаления')
 
-    def __str__(self):
-        return "%s %s, %s" % (self.first_name, self.last_name, self.age)
+    class Meta:
+        verbose_name = 'Дети'
+        verbose_name_plural = 'Дети'
 
 
 class Program(models.Model):
     name = models.CharField(max_length=255, verbose_name="Название программы")
     description = models.TextField(max_length=2000, blank=True, null=True, verbose_name="Описание программы")
-    child = models.ForeignKey(Children, on_delete=models.PROTECT, related_name="child_program")
+    child = models.ForeignKey(Children, on_delete=models.PROTECT, related_name="child_program",
+                              verbose_name='Имя ребенка')
     author_therapist = models.ForeignKey(UserInfo, on_delete=models.PROTECT, related_name='author_program')
     attending_therapist = models.ForeignKey(UserInfo, on_delete=models.PROTECT, related_name='attending_program')
     skill = models.ManyToManyField(Skill, related_name='skill_program')
@@ -67,19 +73,21 @@ class Program(models.Model):
     edited_date = models.DateTimeField(auto_now=True, blank=True, null=True, verbose_name="Дата редактирования")
     deleted_date = models.DateTimeField(blank=True, null=True, verbose_name="Дата удаления")
 
-    def __str__(self):
-        return "Название программы: %s, Исполнитель %s" % (self.name, self.attending_therapist.user.last_name)
+    class Meta:
+        verbose_name = 'Программа'
+        verbose_name_plural = 'Программы'
 
 
 class Session(models.Model):
-    program = models.ForeignKey(Program, verbose_name='Программа', on_delete=models.PROTECT)
+    program = models.ForeignKey(Program, on_delete=models.PROTECT, verbose_name="Программа")
     description = models.TextField(max_length=2000, blank=True, null=True, verbose_name="Описание сессии")
     created_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     edited_date = models.DateTimeField(auto_now=True, blank=True, null=True, verbose_name="Дата редактирования")
     deleted_date = models.DateTimeField(blank=True, null=True, verbose_name="Дата удаления")
 
-    def __str__(self):
-        return 'Сессия номер: %s, Программа: %s, Создано %s' % (self.id, self.program.name, self.created_date)
+    class Meta:
+        verbose_name = 'Сессия'
+        verbose_name_plural = 'Сессии'
 
 
 class Results(models.Model):
@@ -101,5 +109,6 @@ class Results(models.Model):
     edited_date = models.DateTimeField(auto_now=True, blank=True, null=True, verbose_name="Дата редактирования")
     deleted_date = models.DateTimeField(blank=True, null=True, verbose_name="Дата удаления")
 
-    def __str__(self):
-        return 'Сессия %s,  навык %s' % (self.session.id, self.skill.code_skill)
+    class Meta:
+        verbose_name = 'Результат'
+        verbose_name_plural = 'Результаты'
