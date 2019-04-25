8c682cf0d708f043ab628635d19a35f782217487
Zeroitman <borsh130894@gmail.com>
2019-04-26 03:42:37 +0600

    Закончил выполнение тикета #59

--- source/webapp/static/js/main.js
+++ source/webapp/static/js/main.js
@@ -5,3 +5,8 @@ $(function () {
             $('.btn').prop('disabled', false) : $('.btn').prop('disabled', true);
     });
 });
+
+$(function () {
+    (!$(document.getElementById("001")).text()) ? $(document.getElementById("01")).text('Нет выполненных навыков') : null;
+    (!$(document.getElementById("002")).text()) ? $(document.getElementById("02")).text('Нет невыполненных навыков') : null;
+});
