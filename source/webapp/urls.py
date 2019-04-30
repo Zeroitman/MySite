342b58eda279037007d2cc025660486d18e93614
Maks <Rrnn941@gmail.com>
2019-04-30 13:10:04 +0600

    Сделан шаблон child_list с выводом всех детей, сделана форма поиска на странице детей с возможностью поиска по ФИО и характеристике, сделаны проверки на 0 количетсво результатов, на пустые поля в карточке ребенка, сделано к тикету #66

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from webapp.views import ProgramDetailView, ChildList, SessionDetailView, ProgramList, ResultList
+from webapp.views import ProgramDetailView, ChildProgramList, SessionDetailView, ProgramList, ResultList, ChildList, ChildSearchView
 
 app_name = 'webapp'
 
 
 urlpatterns = [
-    path('', ChildList.as_view(), name='child_list'),
+    path('', ChildProgramList.as_view(), name='child_program_list'),
     path('program/', ProgramList.as_view(), name='program_list'),
     path('program/<int:pk>', ProgramDetailView.as_view(), name='program_detail'),
+    path('child/', ChildList.as_view(), name='child_list'),
+    path('search/', ChildSearchView.as_view(), name='search_view'),
     path('current_session/<int:pk>', SessionDetailView.as_view(), name='session_view'),
     path('session_result/<int:pk>', ResultList.as_view(), name='session_result_view'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,11 +1,13 @@
-from webapp.models import Program, Session, Result
-from django.views.generic import ListView, DetailView
+from webapp.models import Program, Session, Result, Child
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, View
+from django.db.models import Q
 
 
 # ChildList - главная страница с выводом детей
-class ChildList(ListView):
+class ChildProgramList(ListView):
     model = Program
-    template_name = 'child_list.html'
+    template_name = 'child_program_list.html'
 
 
 # ProgramList - страница вывода всех программ
@@ -30,3 +32,31 @@ class ProgramDetailView(DetailView):
 class SessionDetailView(DetailView):
     model = Session
     template_name = 'session_detail.html'
+
+
+# ChildList - страница выводящая список всех детей, без привязки к программе
+class ChildList(ListView):
+    model = Child
+    template_name = 'child_list.html'
+
+
+# ChildSearch - метод поиска по детям на странице детей, после отправки формы с именем либо фамилией ребенка
+# Перекидывает на страницу child_search_results где подсчитывается количество результатов
+# И все результаты в виде ListGroup(Bootstrap)
+class ChildSearchView(View):
+    template_name = 'child_search_results.html'
+
+    def get(self, request, *args, **kwargs):
+        query = self.request.GET.get('q')
+        # TODO сделать проверку на пустое поле, возможно сделать кнопку disabled при длине input'a <= 1.
+        searched_child = Child.objects.filter(
+            Q(first_name__icontains=query) |
+            Q(last_name__icontains=query) |
+            Q(third_name__icontains=query) |
+            Q(characteristic__icontains=query))
+        # Q позволяет искать по полям из указанной переменной со всеми детьми в формате Q(__поле из модели Child/
+        # __icontains=query)
+        context = {
+            'searched_child': searched_child
+        }
+        return render(self.request, self.template_name, context)
