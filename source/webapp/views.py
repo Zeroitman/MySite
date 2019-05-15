a06ad2c8215bd59602e1f496c73101af0f9cebbe
Zeroitman <borsh130894@gmail.com>
2019-05-15 15:46:38 +0600

    Тикет #103. Вывел процент открытых навыков

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -162,6 +162,20 @@ class ProgramDetailView(DetailView):
     model = Program
     template_name = 'program_views/program_detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now_pk = int(self.kwargs.get('pk'))
+        all_true_skills = SkillsInProgram.objects.filter(program_id=now_pk, status=True)
+        all_session_in_program = Session.objects.filter(program_id=now_pk, status_session=True)
+        skills = {}
+        for session in all_session_in_program:
+            for sk in all_true_skills:
+                current_program_skill = Result.objects.filter(session_id=session.pk, skill_id=sk)
+                for skill in current_program_skill:
+                    skills[skill.skill_id] = str(skill.percent)
+        context['all_skill'] = skills
+        return context
+
 
 class ProgramCreateView(CreateView):
     model = Program
